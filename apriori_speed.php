0295d31af4765ded8954bf41e9a1475b65e8cf17
yaoyang <kiki@smarty.so>
2018-03-15 12:53:19 +0800

    添加部分注释

--- apriori_speed.php
+++ apriori_speed.php
@@ -60,8 +60,8 @@ function cal_phase_make_up_counts($records, $phase_num, $rule_delimiter)
 }/*}}}*/
 
 /**
- * apriori 
- * 
+ * apriori
+ *
  * @param array $records  数据记录
  * @param int $max_phase  项中元素的最大个数
  * @param float $min_support  最小支持度
@@ -99,7 +99,7 @@ function apriori(array $records, int $max_phase = 3, float $min_support = 0.1, f
                 $make_up_counts[$phase_num][$rule] = $count;
 
                 // 计入支持度数组
-                $supports[$phase_num][$rule] = 
+                $supports[$phase_num][$rule] =
                     // 支持度逻辑
                     $support = $count / $record_count;
 
@@ -110,6 +110,7 @@ function apriori(array $records, int $max_phase = 3, float $min_support = 0.1, f
 
                     $max_confidience_from_rule_count = $count / $min_confidience;
 
+                    // 拆解 rule 遍历得出其上一阶的项
                     $rule_item = explode($rule_delimiter, $rule);
 
                     for ($i = 0; $i < $phase_num; $i ++) {
@@ -124,20 +125,26 @@ function apriori(array $records, int $max_phase = 3, float $min_support = 0.1, f
 
                             $from_rule_count = $make_up_counts[$last_phase][$from_rule];
 
+                            // 上一阶项与当前项计算满足最小置信度的数据
                             if ($from_rule_count <= $max_confidience_from_rule_count) {
 
                                 if (! array_key_exists($from_rule, $confidiences[$last_phase])) {
                                     $confidiences[$last_phase][$from_rule] = [];
                                 }
 
-                                $confidiences[$last_phase][$from_rule][$rule] = 
+                                // 计入置信度数组
+                                $confidiences[$last_phase][$from_rule][$rule] =
+                                    // 置信度逻辑
                                     $confidience = $count / $from_rule_count;
 
                                 if (! array_key_exists($from_rule, $lifts[$last_phase])) {
                                     $lifts[$last_phase][$from_rule] = [];
                                 }
 
-                                $lifts[$last_phase][$from_rule][$rule] = $confidience / $support;
+                                // 计入提升度数组
+                                $lifts[$last_phase][$from_rule][$rule] =
+                                    // 提升度逻辑
+                                    $lift = $confidience / $support;
                             }
                         }
 
